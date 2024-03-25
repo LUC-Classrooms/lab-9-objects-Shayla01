8c847d79c22f72430b0400f677ee9a4383b22a1e
Shayla01 <157332439+Shayla01@users.noreply.github.com>
2024-03-25 03:28:04 +0000

    Lab 9 Complete

--- script.js
+++ script.js
@@ -12,16 +12,20 @@ function setup() {
   assign the result of this function to myObj1 and then to myObj2
   */
 
-myObj1 = new MyClass(100, 100);
-myObj2 = new MyClass(100, 200);
+myObj1 = new MyClass(width/2, height/2);
+myObj2 = new MyClass(20, 100);
 console.log(myObj1)
 }
 
 function draw() {
   background(200);
   // add code here to make your objects move and display on canvas
-  myObj1.diplay(); 
+  myObj1.display(); 
+  myObj1.move();
+  myObj1.r += .01;
   myObj2.display(); 
+  myObj2.move();
+  myObj2.r += -.02;
 }
 
 //Class constructor:
@@ -34,6 +38,7 @@ function MyClass(tempX, tempY){
   this.xSpeed = random (-1.0, 1.0); // each instance moves in a different direction
   this.ySpeed = random (-1.0, 1.0);
   this.color = color(random(255), random(255), random(255));
+  this.r = 0;
   
   //some basic object methods
   this.move = function (){
@@ -41,7 +46,7 @@ function MyClass(tempX, tempY){
     this.y += this.ySpeed;
 
     if(this.x > width || this.x< 0) {
-      this.ySpeed *= -1; 
+      this.xSpeed *= -1; 
     }
     
     if(this.y > height || this.y < 0){
@@ -54,11 +59,13 @@ function MyClass(tempX, tempY){
     
     push(); // create a transparency layer for the object
     translate(this.x, this.y); //shift the canvas (0,0) to the object location
+    rotate(this.r);
     fill(this.color); // set the color
-    ellipse(0, 0, this.d/2, this.d);
+    ellipse(0, 0, this.d, this.d);
+    fill(200);
+    ellipse(0, 0 + this.d/4, this.d/4, this.d/4);
     rectMode(CENTER); 
-    rect(0, -this.d/2, this.d/4)
-    rect(0, -this.d/2 + this.d/8., this.d, 3)
+    rect(0, 0 -this.d/2, this.d, this.d/2);
     // note that when you want to use the objects properties, you need to use "this"
     // add more drawing code to make your image a little more complex
 
